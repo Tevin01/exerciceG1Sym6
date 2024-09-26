f29e2621a38d7ea5e50ebccfaa91b3b965bd3711
Tevin01 <tevin.eliya.bofona@cf2000.onmicrosoft.com>
2024-09-26 16:29:37 +0200

    Twiiiiiiiig

---
+++ src/Controller/MainController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'title' => 'Homepage',
+            'homepage_text'=> "Nous somme le ".date('d/m/Y \à H:i'),
+            
+        ]);
+    }
+}
+
+
